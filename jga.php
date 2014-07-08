51d75b45c1dc599aacedbbd7f4d6b3cd87326153
Jordan Hayes <jordan@sameplace.com>
2014-07-08 16:27:07 +0000

    get-attachment using json

---
+++ jga.php
@@ -0,0 +1,25 @@
+<?php
+include("config.php");
+
+if (empty($_REQUEST['oid'])) {
+    header('Content-Type: application/json;charset=utf-8;');
+    echo json_encode('Error: must specify an oid!');
+    return;
+}
+$oStr = $_REQUEST['oid'];
+
+$a = new spAttachment();
+if (! $a->inflate($oStr)) {
+    header('Content-Type: application/json;charset=utf-8;');
+    echo json_encode("Error: cannot find Attachment='".htmlentities($oStr)."'");
+    return;
+}
+
+header('Content-Type: '.$a->m_mType);
+header('Content-Disposition: attachment; filename="'.$a->m_name.'"');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Content-Length: '.filesize($a->m_path));
+flush();
+readfile($a->m_path);
+?>
