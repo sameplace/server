37eef765cab95a8191581e1f683718574ef1b966
Jordan Hayes <jordan@sameplace.com>
2014-07-08 17:30:37 +0000

    Allow override of config stuff in config-local.php

--- config.php
+++ config.php
@@ -5,6 +5,10 @@ require_once 'utils.php';
 require_once 'spfuncs.php';
 require_once 'oid.php';
 
+$spDbName = "sqlite:/ext/www/public/archie.sqb";
+
+@include("config-local.php");
+
 // should not have to change anything below, but you might want to
 
 $defDealSpace = '_____defaultDealSpace_____';
