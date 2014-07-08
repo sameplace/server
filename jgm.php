8fd7d9fa7fcfdaece98c508e8f84e29e4fb9e5f1
Jordan Hayes <jordan@sameplace.com>
2014-07-08 16:27:27 +0000

    get-mimedoc with json

---
+++ jgm.php
@@ -0,0 +1,39 @@
+<?php include("spauth.php"); ?>
+<?php
+include("config.php");
+
+header('Content-Type: application/json;charset=utf-8;');
+
+$me = spUser::lookupMe();
+if (null == $me) {
+    echo json_encode('Error: You do not belong here! 1');
+    return;
+}
+
+if (empty($_REQUEST['oid'])) {
+    echo json_encode('Error: You do not belong here! 2');
+    return;
+}
+$oid = $_REQUEST['oid'];
+
+$md = new spMimeDoc;
+if (! $md->inflate($oid)) {
+    echo json_encode('Error: You do not belong here! 3');
+    return;
+}
+if (! $me->isAdmin() && $md->m_owner != $me->getOid()) {
+    echo json_encode('Error: You do not belong here! 4');
+    return;
+}
+
+$ret = array();
+$ret[] = $md->toJson();
+
+$as = spAttachment::lookupAll($md->getOid());
+if (!empty($as) && 0 != count($as))
+    foreach ($as as $a)
+	$ret[] = $a->toJson();
+
+echo json_encode($ret);
+
+?>
