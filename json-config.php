925791e0a28d630af3c62bc8d240ee3ba7a10e0e
Jordan Hayes <jordan@sameplace.com>
2014-11-11 18:00:49 +0000

    cleanup

--- json-config.php
+++ json-config.php
@@ -2,8 +2,8 @@
 
 include("config.php");
 
-if (! isset($_SESSION)) 
-    session_start(); 
+if (! isset($_SESSION))
+    session_start();
 
 if (! isset($_SESSION['email'])) {
     header("Location: a428.php");
