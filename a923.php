c1bfb5931c99b2608a38481fbcc8e9cba4625dd1
Jordan Hayes <jordan@sameplace.com>
2014-11-13 21:32:26 +0000

    Use new jutils

--- a923.php
+++ a923.php
@@ -4,37 +4,13 @@
 
 include("json-config.php");
 
-$me = spUser::lookupMe();
-if (null == $me) {
-    echo json_encode('Error: You do not belong here!');
+$c = jGetDS("n");
+if (null == $c)
     return;
-}
 
-if (empty($_REQUEST['n'])) {
-    echo json_encode('Error: syntax!');
-    return;
-}
-$n = $_REQUEST['n'];
-
-if (empty($_REQUEST['oid'])) {
-    echo json_encode('Error: syntax!');
-    return;
-}
-
-$oid = $_REQUEST['oid'];
-$ds = new spDealSpace;
-if (! $ds->inflate($oid)) {
-    echo json_encode('Error: You do not belong here!');
-    return;
-}
-if (! $me->isAdmin() && $ds->m_owner != $me->getOid()) {
-    echo json_encode('Error: You do not belong here!');
-    return;
-}
-
-if ($ds->changeName($n))
-    echo json_encode($ds->toJsonUpdated());
+if ($c->ds->changeName($c->n))
+    echo json_encode($c->ds->toJsonUpdated());
 else
-    echo json_encode('Error: You do not belong here!');
+    return jError();
 
 ?>
