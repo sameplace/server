b607495984922c3052f71ef005bce8d3c38ae87f
Jordan Hayes <jordan@sameplace.com>
2014-11-14 06:20:15 +0000

    Get participants from DealSpace

---
+++ a274.php
@@ -0,0 +1,24 @@
+<?php
+
+// a274 oid=[dealspace] p=[partys]
+
+include("json-config.php");
+
+$c = jGetDS("p");
+if (null == $c)
+    return;
+
+$which = array();
+$parts = explode(',', $c->p);
+foreach ($parts as $part)
+    $which[$part] = $part;
+$which = array_keys($which);
+$ret = array();
+$ps = spParticipant::lookupAll($c->ds);
+foreach ($ps as $party)
+    if (in_array($party->m_oStr, $which))
+	$ret[] = $party->toJson();
+
+echo json_encode($ret);
+
+?>
