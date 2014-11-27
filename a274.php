4ff29c0ef770b5d5a5c452f0534d3b303f447407
Jordan Hayes <jordan@sameplace.com>
2014-11-27 21:42:55 +0000

    Use new parse/filter

--- a274.php
+++ a274.php
@@ -2,22 +2,20 @@
 
 // a274 oid=[dealspace] p=[partys]
 
-include("json-config.php");
+require_once 'json-config.php';
 
 $c = jGetDS("p");
 if (null == $c)
     return;
 
-$which = array();
-$parts = explode(',', $c->p);
-foreach ($parts as $part)
-    $which[$part] = $part;
-$which = array_keys($which);
-$ret = array();
+$w = jParseOidList($c->p);
+if (null == $w)
+    return;
+
 $ps = spParticipant::lookupAll($c->ds->getOid());
-foreach ($ps as $party)
-    if (in_array($party->m_oStr, $which))
-	$ret[] = $party->toJson();
+$ret = jFilterOids($w, $ps);
+if (null == $ret)
+    return;
 
 echo json_encode($ret);
 
