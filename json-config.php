767b65c997723e2f1357e9151660ae893a135d09
Jordan Hayes <jordan@sameplace.com>
2015-03-02 19:35:43 +0000

    Watch out for bogus session IDs

--- json-config.php
+++ json-config.php
@@ -3,8 +3,13 @@
 require_once 'config.php';
 require_once 'jutils.php';
 
-if (! isset($_SESSION))
-    session_start();
+if (! isset($_SESSION)) {
+    $ok = @session_start();
+    if (! $ok) {
+	session_regenerate_id(true);
+	session_start();
+    }
+}
 
 if (! isset($_SESSION['email'])) {
     header("Location: a428.php");
